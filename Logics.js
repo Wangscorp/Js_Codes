9a690068c1995f7ec9762982e59bf0c5162fa9e1
Edwin Wang <edwinwang62@gmail.com>
2025-07-19 11:57:52 +0300

    Added Logical Operators

---
+++ Logics.js
@@ -0,0 +1,11 @@
+let condition1 = false;
+let condition2 = true;
+let result = condition1 && condition2;
+console.log(result);
+
+let cost = 50;
+let sellPrice = 60;
+const profit = sellPrice - cost;
+console.log(profit);
+result = cost < sellPrice && profit > 0;
+console.log("result: " + result);
